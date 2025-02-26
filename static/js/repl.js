9f7575d8e8a1be1a2caf65dd9fecf672cc277465
nico <nico@nicos-MacBook-Pro.local>
2025-02-26 12:59:43 -0500

    Fixed hashing for export

--- static/js/repl.js
+++ static/js/repl.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .replace(/=+$/, '')
             .replace(/\+/g, '-')
             .replace(/\//g, '_');
-        let urlString = window.location.href + "#" + hashedCode;
+        let urlString = (window.location.href.slice(0,-1) + "#" + hashedCode);
         navigator.clipboard.writeText(urlString);
         console.log(urlString);
     }
