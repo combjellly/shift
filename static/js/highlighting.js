ebdf5d0007e2896196cdf1d01ea9b74f9c4f3c40
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-02-26 18:17:31 -0500

    added colours to syntax highlighting

---
+++ static/js/highlighting.js
@@ -0,0 +1,69 @@
+const inputEl = document.querySelector('[data-el="input"]');
+const highlightEl = document.querySelector('[data-el="highlight"]');
+const outputEl = document.querySelector('[data-el="output"]');
+
+const converter = new showdown.Converter({
+    metadata: true,
+    parseImgDimensions: true,
+    strikethrough: true,
+    tables: true,
+    ghCodeBlocks: true,
+    smoothLivePreview: true,
+    simpleLineBreaks: true,
+    emoji: true,
+});
+
+const resizeTextarea = (textArea) => {
+    if (!textArea) {
+        return;
+    }
+
+
+    window.requestAnimationFrame(() => {
+        textArea.style.height = 0;
+        if (textArea.scrollHeight > 0) {
+            textArea.style.height = `${textArea.scrollHeight + 2}px`;
+        }
+    });
+};
+
+const highlight = () => {
+    window.requestAnimationFrame(() => {
+        hljs.registerLanguage('shift', function (hljs) {
+            return {
+                // Define keywords, literals, etc.
+                keywords: {
+                    keyword: 'if else while for function every',
+                    literal: 'true false'
+                },
+                contains: [
+                    hljs.COMMENT('//', '$'),
+                    hljs.COMMENT('/\\*', '\\*/'),
+                    {
+                        className: 'string',
+                        begin: /"/, end: /"/
+                    }
+                ]
+            };
+        });
+        const highlighted = hljs.highlight(
+            "shift",
+            inputEl.value
+        ).value;
+        highlightEl.innerHTML = highlighted;
+    });
+};
+
+const init = () => {
+    inputEl.addEventListener("input", () => {
+        resizeTextarea(inputEl);
+        highlight();
+    });
+    inputEl.setAttribute('data-initialized', true);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    init();
+    resizeTextarea(inputEl);
+    highlight();
+})
