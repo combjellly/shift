08b15c522cdf17cd3c7a185d2c80674dc6e764d4
nico <nico@nicos-MacBook-Pro.local>
2025-08-29 18:34:25 -0400

    added clear mute buttons on right of every loops, changed some presets

--- static/js/presets.js
+++ static/js/presets.js
@@ -1,4 +1,75 @@
 var presets = {
+
+	script0: `-- slowcore
+
+rideamp =  [ 50 40 40 40 50 40 40 40 ]
+snareamp = [  0  0 55  0  0  0  55 30 ]
+kickamp =  [  30  0  0 40 50  0  0  0 ]
+counter = 0
+
+note = 0
+melody = [0 2 9 5 4 0 0]
+guitarmelody = [ 0 0 0 0 0 0 2 2 2 7 7 5 ]
+guitarlead = [0 0 0 0 2 2 7 7 5 5 0 0]
+sustainmelody = [7 5 0 2]
+sustaincounter = 0
+
+time = 1
+mod = 0
+
+-- some midi in this, channels 1, 2 and 5
+
+
+
+every 0.5 * time
+	-- sequencer 'clock'
+
+	counter = counter + 1
+	if counter >= 8 [ counter = 0]
+
+	--stereo!! 
+	play.ride:9[(rideamp:counter), 0, 70 ]
+	play.ride:2[(rideamp:counter), 0, 30 ]
+
+	play.snare:9[(snareamp:counter) + 20, 0, 70]
+	play.snare:11[(snareamp:counter) + 10, 0,30]
+
+	play.kick:10[(kickamp:counter) + 30,0,70]
+	play.kick:9[(kickamp:counter) + 30,0,30]
+
+
+
+----- melody
+
+every 3
+	note = guitarmelody:counter
+	play.guitar:0[60,0,50]
+	--midi[60,60,50]
+
+every 2
+	leadnote = guitarlead:counter
+	play.guitar:leadnote[60,0,50]
+	midi:1[60+leadnote,60,200]
+
+every 4
+	--counter = counter + 1
+	if counter == 9 [ counter = 0 ]
+	play.guitar:mod+note[60,0,30]
+	midi:1[48+note,60,200]
+
+every 1
+	hatvolume = 50
+	guitarcounter = guitarcounter + 1
+	if guitarcounter == 12 [ guitarcounter = 0]
+	guitarlead1 = guitarmelody:guitarcounter
+	play.guitar:guitarlead1[60,(-19),50]
+	midi[60+guitarlead1,60,200]
+
+every 16
+	sustaincounter = sustaincounter + 1
+	if sustaincounter >= 4 [ sustaincounter = 0]
+	midi:4[72+(sustainmelody:sustaincounter),60,4000]
+	`,
     script1: `y=0
 every 1.5
 	y = 0
@@ -15,7 +86,10 @@ every 0.75
 	play.piano:(i:y)-1[70,0,100]
     `,
 
-    script2: `change = 6
+    script2: `-- danceeee
+
+
+change = 6
 time = 1/1.3
 
 every 0.75 / time
@@ -63,32 +137,53 @@ every 0.75*2/ time
 	play.hi:(o*change)-1[50,0,0]
     `,
 
-    script3: `i=3
-every y
-	x = random. 1 3
-	y = x / 3
-	
-	play.hi[70,x,p]
-	play.hi:1[70,x+4,o]
+    script3: ` -- dronezzzzz
+pan = 0 
 
-every 0.1
-	play.pingu:x-1[60,x,p]
+notes = [ 0 2 4 7 12 ]
 
-every 0.2 
-	p = random. 0 100
-	o = random. 0 100
-	d = random. 0 100
+p = 0
+pc = 0
+pnotes = [ 0 0 0 7 12 9 7 0]
 
-every 0.05
-	play.organ[50,0+i,p]
-	play.organ[50,7+i,o]
-	play.organ1[50,12+i,d]
+
+every 8
+	pc = pc + 1
+	if pc == 8 [ pc = 0 ]
+
+every 0.25 
+	pan = random. 0 100
+	play.organ[50, 12 + (pnotes:pc) , pan]
+
+every 0.25 
+	pan = random. 0 100
+	play.organ[50, 24 + (pnotes:pc) , pan]
+
+every 0.25 
+	pan = random. 0 100
+	play.organ[50, 19 + (pnotes:pc), pan]
 
 every 16
-	i= x
+	if counter == 5 [ counter = 0] 
+	counter = counter + 1
+ 	play.bird:notes:counter[100, 0 ,50,2]
+
+every 8
+	play.piano:4[ 90, -24 + p, 50, 4]
+
+every 0.25 
+	play.organ[60,notes:counter,pan]
+
+every 0.125 
+	play.organ[60,(notes:counter)+ 12,pan]
+
+
     `,
 
-    script4: `i=0
+    script4: `-- midi example
+-- midi[note,velocity,duration] (notes up to 127, velocity up to 100, duration in ms)
+
+i=0
 notes = [1 6 10 13 18]
 note = 3
 pCounter = 0 
@@ -96,7 +191,7 @@ pnote = 0
 
 every 0.5
 	pnote = notes:pCounter
-	midi[pnote+ 50,60,300]
+	midi[pnote+ 50,60,300] -- here is the midi code :-)
 	pCounter = pCounter + 1
 	if pCounter == 5 [ pCounter = 0]
     `
